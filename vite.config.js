8f5a142cb12b545377fa53ded62d3ddd965f7f6e
C-West8 <CameronTWest@gmail.com>
2024-08-19 08:30:34 -0500

    fixed base in config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/c-west8.github.io/',
+  base: '/CWestSoftware/',
   plugins: [react()],
 })
